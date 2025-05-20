c07b36b542b4e7a779805fee5ccb1f9c033fb596
swordlegend32 <giopro1986@gmail.com>
2025-05-20 19:07:18 +0400

    Server + Backend

--- server.py
+++ server.py
@@ -1,7 +1,20 @@
-# main.py
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.websockets import WebSocket
 
 app = FastAPI()
+
+# Serve static files (JS, CSS)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# HTML rendering
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def get_home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
